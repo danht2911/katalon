9db3144f019fe4bc7891cc711c3b82322d09aa99
Tran Thanh Danh <ttdanh@TMA.COM.VN>
2024-08-02 18:10:15 +0700

    August 2 2nd

---
+++ My First Web UI Project/Object Repository/covid19.gov.vn/columnHead.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>columnHead</name>
+   <tag></tag>
+   <elementGuidId>6deef952-0f69-43a8-8788-1f46e627c239</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;row head&quot;//span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/covid19.gov.vn/iframe1</value>
+      <webElementGuid>c24138e7-b101-4ac1-baa4-8505d7a87124</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ My First Web UI Project/Object Repository/covid19.gov.vn/iframe1.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>iframe1</name>
+   <tag></tag>
+   <elementGuidId>c28484ce-bba8-44fe-94dd-7975647f0595</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;main home-page &quot;]//div[@class=&quot;home__juncture&quot;]//iframe</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
